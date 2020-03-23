9a85777dc320abaef70ab067a99519778634bcf2
Rafael Santos <formigarafa@gmail.com>
2020-03-23 23:59:38 +1300

    Single field type for dynamic form

--- app/components/new-report-form.js
+++ app/components/new-report-form.js
@@ -1,6 +1,31 @@
 import Component from '@glimmer/component';
-import { action } from "@ember/object";
+import { action, set } from "@ember/object";
+
+const FieldsMapping = {
+  'ShortTextBox': 'string',
+}
+
 export default class NewReportFormComponent extends Component {
+  formData = {};
+  constructor(owner, args) {
+    super(owner, args);
+
+    this.args.fields.forEach((f) => {
+      if (FieldsMapping[f.field_type]) {
+        set(this.formData, f.key, f.value);
+      }
+    })
+  }
+
+  get sortedFields() {
+    return this.args.fields.map((f) => {
+      const fieldType = FieldsMapping[f.field_type];
+      if (fieldType) {
+        return {...f, fieldType};
+      }
+    }).compact().sortBy('form_order');
+  }
+
   @action
   send() {
     console.log("sending", {channel: this.args.channel, fields: this.args.fields});

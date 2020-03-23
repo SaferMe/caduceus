d30b250c4f37da33320b8744547f75f4bd73ba95
Rafael Santos <formigarafa@gmail.com>
2020-03-24 00:38:12 +1300

    sending report creation requests

--- app/components/new-report-form.js
+++ app/components/new-report-form.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { action, set } from "@ember/object";
+import fetch from 'fetch';
 
 const FieldsMapping = {
   'ShortTextBox': 'string',
@@ -28,7 +29,39 @@ export default class NewReportFormComponent extends Component {
   }
 
   @action
-  send() {
+  async send() {
     console.log("sending", {channel: this.args.channel, fields: this.args.fields});
+    const body = Object.assign({}, this.formData, {
+      account_id: this.args.channel.id,
+      geom: "Point(174.868709 -41.213559)",
+      address: "lat: -41.213559, long: 174.868709",
+    });
+    let report, errors;
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/v4/reports`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token token=public',
+            'X-AppId': 'com.thundermaps.saferme',
+            'X-InstallationId': 'generic-public-access',
+          },
+          body: JSON.stringify(body),
+        }
+      );
+      if (response.ok) {
+        report = await response.json();
+      }
+      else if (response.status === 422) {
+        // validation errors
+        errors = await response.json();
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+    console.log(report, errors);
   }
 }

1de59288311b13b62981b79e3c0fd2376f22d31b
Rafael Santos <formigarafa@gmail.com>
2020-03-23 17:41:24 +1300

    dynamically picking field components for each form field

--- app/components/channel-form/form-field.js
+++ app/components/channel-form/form-field.js
@@ -5,6 +5,6 @@ const FieldTypeMapping = {
 
 export default class ChannelFormFormFieldComponent extends Component {
   get fieldType() {
-    return FieldTypeMapping[this.args.field.field_type] || "NullField"
+    return FieldTypeMapping[this.args.field.field_type] || "channel-form/null-field"
   }
 }

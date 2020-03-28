48dd659ad101cffa240f7c53d433e3c574196139
Rafael Santos <formigarafa@gmail.com>
2020-03-28 23:58:36 +1300

    implement google place autocomplete search input using power select

---
+++ app/components/inputs/power-place-autocomplete-input.js
@@ -0,0 +1,55 @@
+import Component from '@glimmer/component';
+import ObjectProxy from '@ember/object/proxy';
+import { action, get, set } from '@ember/object';
+import { isBlank } from '@ember/utils';
+import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
+import { resolve } from 'rsvp';
+import { inject as service } from '@ember/service';
+
+export default class NewReportFormComponent extends Component {
+  @service('google-place-autocomplete') googlePlaceAutocompleteService;
+
+  constructor() {
+    super(...arguments);
+  }
+
+  @action
+  async select(newValue) {
+    if (isBlank(newValue)) {
+      set(this, 'args.config.value', {});
+      return;
+    }
+
+    let googleRequest = {
+      placeId: newValue.place_id,
+      fields: ['geometry.location'],
+    };
+    let placeDetails = await this.googlePlaceAutocompleteService.getDetails(googleRequest);
+
+    if (get(placeDetails, 'geometry.location')) {
+      newValue.location = {
+        lat: placeDetails.geometry.location.lat(),
+        lng: placeDetails.geometry.location.lng(),
+      };
+    }
+
+    set(this, 'args.config.value', newValue);
+  }
+
+  get selectedItem() {
+    return this.args.config.value;
+  }
+
+  @action
+  searchPlaces(placeServiceInput) {
+    if (isBlank(placeServiceInput)) {
+      return [];
+    }
+    let properties = {
+      input: placeServiceInput,
+      types: ["address"],
+      offset: 3,
+    };
+    return this.googlePlaceAutocompleteService.getPlacePredictions(properties);
+  }
+}
